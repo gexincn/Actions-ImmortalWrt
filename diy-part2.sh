c2f44d49bacffbb7caeef686923642cffb1da789
gexincn <50611830+gexincn@users.noreply.github.com>
2024-12-01 10:20:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,9 +19,8 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# Modify DISTRIB_DESCRIPTION
-BUILD_DATE=$(date +'%Y.%m.%d')
-sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='%D %A %V Compiled by Nomex, $BUILD_DATE'|g" package/base-files/files/etc/openwrt_release
+# Modify OPENWRT_RELEASE
+sed -i "s|OPENWRT_RELEASE='.*'|OPENWRT_RELEASE='%D %A %V Compiled by Nomex,%B'|g" package/base-files/files/usr/lib/os-release
 
 # golang
 rm -rf feeds/packages/lang/golang
