d495a0b102193dde5729174064a2efa67c2929a2
gexincn <50611830+gexincn@users.noreply.github.com>
2024-12-03 00:12:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,32 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.8/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# Modify OPENWRT_RELEASE
-#BUILD_DATE=$(date +'%Y.%m.%d')
-#sed -i "s|DISTRIB_DESCRIPTION='%D %V %C'|DISTRIB_DESCRIPTION='%D %A %V Compiled by Nomex, $BUILD_DATE'|g" package/base-files/files/etc/openwrt_release
-#sed -i "s|OPENWRT_RELEASE='%D %V %C'|OPENWRT_RELEASE='%D %A %V Compiled by Nomex,$BUILD_DATE'|g" package/base-files/files/usr/lib/os-release
-
-# Modify ImmortalWrt_RELEASE
-#BUILD_DATE=$(date +'%Y.%m.%d')
-sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='Compiled by Nomex'" >> /etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='ImmortalWrt 24.10'" >> /etc/openwrt_release
-echo "" >> package/emortal/default-settings/files/99-default-settings
-cat fix_file >> package/emortal/default-settings/files/99-default-settings
-rm fix_file
-
-# 添加编译日期前缀到固件文件名
-sed -i '/IMG_PREFIX:=/a\BUILD_DATE_PREFIX := $(shell date +"%Y%m%d")' include/image.mk
-sed -i "s/IMG_PREFIX:=/IMG_PREFIX:=${BUILD_DATE_PREFIX} /g" include/image.mk
 
 # golang
 rm -rf feeds/packages/lang/golang
