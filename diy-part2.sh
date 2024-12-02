8c4a8091ffbb638f85867cc33e2ee7ebba505bcd
gexincn <50611830+gexincn@users.noreply.github.com>
2024-12-02 21:58:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,6 +24,18 @@
 #sed -i "s|DISTRIB_DESCRIPTION='%D %V %C'|DISTRIB_DESCRIPTION='%D %A %V Compiled by Nomex, $BUILD_DATE'|g" package/base-files/files/etc/openwrt_release
 #sed -i "s|OPENWRT_RELEASE='%D %V %C'|OPENWRT_RELEASE='%D %A %V Compiled by Nomex,$BUILD_DATE'|g" package/base-files/files/usr/lib/os-release
 
+# Modify ImmortalWrt_RELEASE
+BUILD_DATE=$(date +'%Y.%m.%d')
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_REVISION='Compiled by Nomex,$BUILD_DATE'" >> /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='ImmortalWrt %A'" >> /etc/openwrt_release
+echo "" >> package/emortal/default-settings/files/99-default-settings
+cat fix_file >> package/emortal/default-settings/files/99-default-settings
+
+# 删除临时文件
+rm fix_file
+
 # 添加编译日期前缀到固件文件名
 sed -i '/IMG_PREFIX:=/a\BUILD_DATE_PREFIX := $(shell date +"%Y%m%d")' include/image.mk
 sed -i "s/IMG_PREFIX:=/IMG_PREFIX:=${BUILD_DATE_PREFIX} /g" include/image.mk
