a2bc1cfa36d2812bcb055835a785ec635cdf55e6
gexincn <50611830+gexincn@users.noreply.github.com>
2024-12-02 22:02:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,7 +29,7 @@ BUILD_DATE=$(date +'%Y.%m.%d')
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_REVISION='Compiled by Nomex,$BUILD_DATE'" >> /etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='ImmortalWrt %A'" >> /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='ImmortalWrt %V'" >> /etc/openwrt_release
 echo "" >> package/emortal/default-settings/files/99-default-settings
 cat fix_file >> package/emortal/default-settings/files/99-default-settings
 rm fix_file
