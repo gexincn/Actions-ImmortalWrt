b886f551626b256739df2f13ae192c1e31f2e555
gexincn <50611830+gexincn@users.noreply.github.com>
2025-05-10 11:06:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
 
 # mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
