2ad9006b2456d8049fcbd31d5c60c807a78c7c14
gexincn <50611830+gexincn@users.noreply.github.com>
2025-05-16 22:06:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,20 +17,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 删除已有 passwall 相关 feed（避免重复）
-sed -i "/passwall/d" feeds.conf.default
-sed -i "/helloworld/d" feeds.conf.default
-
 # 添加最新官方 passwall 源
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages" >> feeds.conf.default
-
-# 更新 feeds（包含 passwall）
-./scripts/feeds update -a
-
-# 安装 passwall 所有包
-./scripts/feeds install -a -p passwall
-./scripts/feeds install -a -p passwall_packages
 
 # mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
