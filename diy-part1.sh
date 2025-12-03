4c06870be765f9f69e17738c6bf1541ceca3df66
gexincn <50611830+gexincn@users.noreply.github.com>
2025-12-03 21:51:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,23 +10,16 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 删除 feeds.conf中的passwall相关项，避免feeds拉取
+# 删除 feeds.conf 中的 passwall 项，避免 feeds 拉取旧版本
 sed -i '/passwall/d' feeds.conf.default
 
-# 强制使用最新版Passwall（直接 clone 源码）
+# 强制使用最新版 Passwall（直接 clone 源码）
 rm -rf package/passwall
 rm -rf package/passwall_packages
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall_packages
 
-# mosdns
+# mosdns（移除旧 geodata & mosdns）
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
@@ -37,3 +30,8 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf package/openclash
 git clone https://github.com/vernesong/OpenClash.git package/openclash
+
+# 禁止 OpenClash 自动下载核心（避免 GHA /tmp 爆满）
+# 只保留空目录，运行后设备会自动下载核心
+rm -rf package/openclash/luci-app-openclash/root/etc/openclash/core
+mkdir -p package/openclash/luci-app-openclash/root/etc/openclash/core
